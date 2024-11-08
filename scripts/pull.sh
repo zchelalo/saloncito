818a1ffa20edbaa50cab0c8febdb933415398794
zchelalo <eduardosaavedra687@gmail.com>
2024-11-07 21:48:30 -0700

    update: all submodules

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -5,12 +5,18 @@ git pull origin main
 
 cd ../sa_user
 git pull origin main
-
-cd ../sa_auth
+cd ./sa_proto
 git pull origin main
 
-cd ../sa_api_gateway
+cd ../../sa_auth
+git pull origin main
+cd ./sa_proto
 git pull origin main
 
-cd ../
+cd ../../sa_api_gateway
+git pull origin main
+cd ./sa_proto
+git pull origin main
+
+cd ../../
 git pull origin main
