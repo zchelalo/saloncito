ea5b3ed8fdddc6bb9297466fde99d0318e3f8907
Eduardo Saavedra <eduardo.saavedra@ngxnetworks.com>
2025-02-25 13:36:17 -0700

    fix: stop script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+cd ./sa_user
+make setup
+
+cd ../sa_auth
+npm run setup
+
+cd ../sa_class_management
+make setup
+
+cd ../sa_api_gateway
+make setup
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-docker stop sa_api_gateway_app
+cd ./sa_user
+make compose-down
 
-docker stop sa_class_management_app
-docker stop sa_class_management_adminer
-docker stop sa_class_management_db
+cd ../sa_auth
+npm run compose:down
 
-docker stop sa_auth_app
-docker stop sa_auth_adminer
-docker stop sa_auth_db
+cd ../sa_class_management
+make compose-down
 
-docker stop sa_user_app
-docker stop sa_user_adminer
-docker stop sa_user_db
+cd ../sa_api_gateway
+make compose-down
