3b69edb55de7aa94d4ef2641319ce8ca96935795
Eduardo Saavedra <eduardo.saavedra@ngxnetworks.com>
2025-02-26 11:35:37 -0700

    add: validation to newman script

--- scripts/test_endpoints.sh
+++ scripts/test_endpoints.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 docker run \
+  --rm \
   -v $(pwd)/newman:/etc/newman \
   --network=saloncito \
   -t postman/newman \
   run /etc/newman/collections/saloncito.postman_collection.json \
-  -e /etc/newman/envs/saloncito.postman_environment.json
+  -e /etc/newman/envs/saloncito.postman_environment.json \
+
+if [ $? -ne 0 ]; then
+  echo "Error: al menos una prueba ha fallado."
+  exit 1
+fi
+
+echo "Todas las pruebas pasaron exitosamente."
