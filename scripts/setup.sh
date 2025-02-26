1059f0579c3efed66f3656d9df13ce63f6d34212
Eduardo Saavedra <eduardo.saavedra@ngxnetworks.com>
2025-02-26 13:48:20 -0700

    fix: setup.sh script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 cd ./sa_user
+make create-envs
 sudo make setup
 
 cd ../sa_auth
+npm run create:envs
 npm run setup
 
 cd ../sa_class_management
+make create-envs
 sudo make setup
 
 cd ../sa_api_gateway
+make create-envs
 sudo make setup
